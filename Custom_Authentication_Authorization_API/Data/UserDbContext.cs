675bcd414d83c290be360c99d1b4d9ff5f2d014b
Al-Abir <sheikhabir196@gmail.com>
2026-02-21 13:40:03 +0600

    Database connection

---
+++ Custom_Authentication_Authorization_API/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Custom_Authentication_Authorization_API.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Custom_Authentication_Authorization_API.Data
+{
+    public class UserDbContext :DbContext
+
+    {
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+       
+        public DbSet<User> Users { get; set; }
+    }
+}
